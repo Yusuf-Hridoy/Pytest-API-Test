f0e8ffd78200df31e16a9260dc826a65f5ab3a08
Yusuf-Hridoy <yusuf.cityu.cse@gmail.com>
2026-01-25 20:46:20 +0600

    some changes

--- test/test_createuser.py
+++ test/test_createuser.py
@@ -22,3 +22,24 @@ def test_create_booking():
 
     # print the response code 
     assert response.status_code == 200
+
+    
+
+def test_another_booking():
+    booking_data = {
+        "firstname" : "yusufxx",
+        "lastname" : "jenex",
+        "totalprice" : 111,
+        "depositpaid" : True,
+        "bookingdates" : {
+            "checkin" : "2024-08-23",
+            "checkout" : "2024-08-27"
+        },
+        "additionalneeds" : "Breakfast"
+    }
+    response = requests.post(url, json=booking_data)
+    print(response.status_code) # to see if the request was successful
+    print(response.content) # to see the raw content of the response
+
+    # print the response code 
+    assert response.status_code == 200
